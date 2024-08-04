cf2d2372967d6accb055a8ad98f9b8798e970e99
rida fatima <168473185+Rida-fatima-11@users.noreply.github.com>
2024-08-04 14:24:45 +0500

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,31 @@
+// var counter = 0;
+
+// function increase(){
+//     counter++
+//     document.getElementById("counter").innerHTML = counter
+// }
+// function Reset(){
+//     counter=0
+//     document.getElementById("counter").innerHTML = counter
+// }
+//  function Decrease(){
+//      counter--
+//      document.getElementById("counter").innerHTML = counter
+//  }
+var count =0;
+
+function increase(){
+    count++
+    document.getElementById('counter').innerHTML = count
+
+}
+function reset(){
+    count=0
+    document.getElementById('counter').innerHTML = count
+}
+
+function decrease(){
+    count--
+    document.getElementById('counter').innerHTML = count
+    
+}
